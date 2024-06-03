e4cb9662effa8dfb1e67d8e6c4ae1ba25eca4a99
Nhan <hotrongnhan0687@gmail.com>
2024-06-03 15:23:25 +0700

    Add auto mode fan - UC4

--- web/src/App.js
+++ web/src/App.js
@@ -6,26 +6,29 @@ import { GardenRoomList } from './components/garden/gardenRoomList';
 import { RoomDeviceList } from './components/room/roomDeviceList';
 import { DeviceControl } from './components/device/deviceControl';
 import { LogIn } from './components/login/logIn';
+import { AutoModeProvider } from './components/room/autoModeContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<NavBar />}>
-        <Route index element={<Homepage />} />
-        <Route path='login' element={<LogIn />} />
-        <Route path='gardens'>
-          <Route index element={<GardensList />} />
-          <Route path=':garden_id/rooms'>
-            <Route index element={<GardenRoomList />} />
-            <Route path=':room_id/devices'>
-              <Route index element={<RoomDeviceList />} />
-              <Route path=':device_id' element={<DeviceControl />} />
+    <AutoModeProvider>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Homepage />} />
+          <Route path='login' element={<LogIn />} />
+          <Route path='gardens'>
+            <Route index element={<GardensList />} />
+            <Route path=':garden_id/rooms'>
+              <Route index element={<GardenRoomList />} />
+              <Route path=':room_id/devices'>
+                <Route index element={<RoomDeviceList />} />
+                <Route path=':device_id' element={<DeviceControl />} />
+              </Route>
             </Route>
           </Route>
         </Route>
-      </Route>
 
-    </Routes>
+      </Routes>
+    </AutoModeProvider>
   );
 }
 
