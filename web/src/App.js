f0da5346dec44faf9040cc4b2463ef4a2db5c7dc
Nhan <hotrongnhan0687@gmail.com>
2024-06-11 22:28:52 +0700

    Add AutoModeContext to App.js

--- web/src/App.js
+++ web/src/App.js
@@ -6,25 +6,28 @@ import { GardenRoomList } from "./components/garden/gardenRoomList";
 import { RoomDeviceList } from "./components/room/roomDeviceList";
 import { DeviceControl } from "./components/device/deviceControl";
 import { LogIn } from "./components/login/logIn";
+import { AutoModeProvider } from './components/room/autoModeContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/login' element={<LogIn />} />
-      <Route path="/" index element={<Homepage />} />
-      <Route path="/" element={<NavBar />}>
-        <Route path='gardens'>
-          <Route index element={<GardensList />} />
-          <Route path=":garden_id/rooms">
-            <Route index element={<GardenRoomList />} />
-            <Route path=":room_id/devices">
-              <Route index element={<RoomDeviceList />} />
-              <Route path=":device_id" element={<DeviceControl />} />
+    <AutoModeProvider>
+      <Routes>
+        <Route path='/login' element={<LogIn />} />
+        <Route path="/" index element={<Homepage />} />
+        <Route path="/" element={<NavBar />}>
+          <Route path='gardens'>
+            <Route index element={<GardensList />} />
+            <Route path=":garden_id/rooms">
+              <Route index element={<GardenRoomList />} />
+              <Route path=":room_id/devices">
+                <Route index element={<RoomDeviceList />} />
+                <Route path=":device_id" element={<DeviceControl />} />
+              </Route>
             </Route>
           </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </AutoModeProvider>
   );
 }
 
